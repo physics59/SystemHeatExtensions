b828d68678406a1996e0dd8968ba27c6f01bd0ac
physics59 <212725807+physics59@users.noreply.github.com>
2026-07-02 12:11:35 -0400

    get rid of arbitrary files

--- FusionMCF.cs
+++
@@ -1,5 +0,0 @@
-public class FusionMCF
-{
-    public float plasmaVolume = 0.2f;
-    public float plasmaMass = 0.2f;
-}
--- HelloKSP.cs
+++ HelloKSP.cs
@@ -1,18 +1,10 @@
 using UnityEngine;
 
 [KSPAddon(KSPAddon.Startup.MainMenu, false)]
-public class Fusion : MonoBehaviour
+public class StartUp : MonoBehaviour
 {   
     public void Start()
     {
-        foreach (var part in PartLoader.LoadedPartsList)
-        {
-            if (part.name.Contains("fusion") ||
-                part.name.Contains("Fusion"))
-            {
-                Debug.Log("[Fusion!] detected: " + part.name);
-            }
-        }
-        Debug.Log("[Fusion!] Version 0.0.1 loaded");
+        Debug.Log("[SystemHeatExpansion] Version 0.1.0 loaded");
     }
 }
